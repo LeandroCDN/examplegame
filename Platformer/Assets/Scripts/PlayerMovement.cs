b9a68c54aebead1ad742978cdbe4534067c9455a
LeandroCDN <63496697+LeandroCDN@users.noreply.github.com>
2020-07-08 13:59:25 -0300

    Add project

---
+++ Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D playerRb;
+    public float speed = 1.1f;
+    public float jumpForce = 220f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRb.velocity = new Vector2(Input.GetAxis("Horizontal")* speed, playerRb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        playerRb.AddForce(Vector2.up * jumpForce);
+    }
+}
